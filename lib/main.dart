6e14293630930894093c19859f14b7f5a51fbf82
peyton <peyton.t131@gmail.com>
2021-03-05 22:35:25 -0600

    flutter and the databse can love each other now

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -17,6 +16,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
