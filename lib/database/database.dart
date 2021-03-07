c0f034c67d8a7cb7b5416e7f834b9fc419526795
peyton <peyton.t131@gmail.com>
2021-03-06 18:27:00 -0600

    databse shenanigans

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 //import '';
 
 final databaseReference = FirebaseDatabase.instance.reference();
@@ -20,14 +23,24 @@ DatabaseReference sendData(String test) {
 }
 
 Future<String> readData() async {
+  // String result;
+  // var db = FirebaseDatabase.instance.reference().child("fromPython");
+  // db.once().then((DataSnapshot snapshot){
+  //   Map<dynamic, dynamic> values = snapshot.value;
+  //   values.forEach((key,values) {
+  //     print(values["message"]);
+  //     result = values["message"].toString();
+  //   });
+  // });
   // databaseReference.once().then((DataSnapshot snapshot) {
   //   print('Data : ${snapshot.value}');
   // });
-  String result = (await FirebaseDatabase.instance.reference().child("fromPython/message").once()).value;
+  String result = (await FirebaseDatabase.instance.reference().child("fromPython").once()).value;
 
   //remove to and from python descriptors---------------------------------------------------------------------------------uncomment later
   //databaseReference.child('fromPython').remove();
-  databaseReference.child('toPython').remove();
+  //databaseReference.child('toPython').remove();
   return(result);
 }
+
 
