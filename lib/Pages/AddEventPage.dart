8939cf478a996cf5cf94fa003ecc9d1169f7b5cb
Mary Plana <MaryCharmaineLoiPlana@my.unt.edu>
2021-03-06 22:33:19 -0600

    Editted UI for events

--- lib/Pages/AddEventPage.dart
+++ lib/Pages/AddEventPage.dart
@@ -48,13 +48,12 @@ class _AddEventPage extends State<AddEventPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("add events"),
+        title: Text("New Event"),
       ),
       body: MyCustomForm(),
     );
   }
 }
-
 
 // Create a Form widget.
 class MyCustomForm extends StatefulWidget {
@@ -80,115 +79,218 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Form(
       key: _formKey,
       child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Name of event"),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                name = value;
-                return null;
-              },
-            ),
-            Text("Notes on event"),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                notes = value;
-                return null;
-              },
-            ),
-            Text("Date"),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                date = value;
-                return null;
-              },
-            ),
-            Text("Time"),
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                time = value;
-                return null;
-              },
-            ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Event Name:",
+                style: kHeaderTextStyle,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                      color: Colors.deepPurple[400],
+                      width: 2.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Name',
+                        border: InputBorder.none,
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        name = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Notes:",
+                style: kHeaderTextStyle,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                      color: Colors.deepPurple[400],
+                      width: 2.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Notes',
+                        border: InputBorder.none,
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        notes = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Date:",
+                style: kHeaderTextStyle,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                      color: Colors.deepPurple[400],
+                      width: 2.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Date',
+                        border: InputBorder.none,
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        date = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Time:",
+                style: kHeaderTextStyle,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    border: Border.all(
+                      color: Colors.deepPurple[400],
+                      width: 2.0,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter Time',
+                        border: InputBorder.none,
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        time = value;
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+              ),
 
-            // ToggleButtons(
-            //   children: <Widget>[
-            //     Icon(Icons.check_circle),
-            //   ],
-            //   isSelected: isSelected,
-            //   onPressed: (int index) {
-            //     setState(() {
-            //       isSelected[index] = !isSelected[index];
-            //     });
-            //   },
-            // ),
-            //change icon and color based on category chosen
-            SmartSelect<String>.single(
-                title: 'Category',
-                value: color,
-                choiceItems: options,
-                onChange: (state) => setState(() => color = state.value)
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false
-                  // otherwise.
-                  if (_formKey.currentState.validate()) {
-                    // If the form is valid, display a Snackbar.
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('Processing Data')));
-                    //add data to list
-                    if (color == 'blue'){
-                      catIcon = Icons.home;
-                      catColor = Colors.blue;
-                    } else if (color == 'yellow'){
-                      catIcon = Icons.directions_car;
-                      catColor = Colors.yellow;
-                    } else if (color == 'red'){
-                      catIcon = Icons.free_breakfast;
-                      catColor = Colors.red;
-                    } else if (color == 'green'){
-                      catIcon = Icons.computer;
-                      catColor = Colors.green;
-                    } else {
-                      catIcon = Icons.face;
-                      catColor = Colors.purple;
-                    }
-                    globals.Events.add(
-                        EventCard(
+              // ToggleButtons(
+              //   children: <Widget>[
+              //     Icon(Icons.check_circle),
+              //   ],
+              //   isSelected: isSelected,
+              //   onPressed: (int index) {
+              //     setState(() {
+              //       isSelected[index] = !isSelected[index];
+              //     });
+              //   },
+              // ),
+              //change icon and color based on category chosen
+              SmartSelect<String>.single(
+                  title: 'Category',
+                  value: color,
+                  choiceItems: options,
+                  onChange: (state) => setState(() => color = state.value)),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple[300],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false
+                      // otherwise.
+                      if (_formKey.currentState.validate()) {
+                        // If the form is valid, display a Snackbar.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Processing Data')));
+                        //add data to list
+                        if (color == 'blue') {
+                          catIcon = Icons.home;
+                          catColor = Colors.blue;
+                        } else if (color == 'yellow') {
+                          catIcon = Icons.directions_car;
+                          catColor = Colors.yellow;
+                        } else if (color == 'red') {
+                          catIcon = Icons.free_breakfast;
+                          catColor = Colors.red;
+                        } else if (color == 'green') {
+                          catIcon = Icons.computer;
+                          catColor = Colors.green;
+                        } else {
+                          catIcon = Icons.face;
+                          catColor = Colors.purple;
+                        }
+                        globals.Events.add(EventCard(
                           eventName: name,
                           eventNotes: notes,
                           eventDate: date,
                           eventTime: time,
                           categoryIcon: catIcon,
                           categoryColor: catColor,
-                        )
-                    );
-                    setState(() {});
-                  }
-                },
-                child: Text('Submit'),
+                        ));
+                        setState(() {});
+                      }
+                    },
+                    child: Text('Submit'),
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
