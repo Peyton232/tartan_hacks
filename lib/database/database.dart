7a011f23b75ac5fb168c35812dd9ee09503bdc1e
peyton <peyton.t131@gmail.com>
2021-03-06 18:58:10 -0600

    database fully functional now

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -35,8 +35,8 @@ Future<String> readData() async {
   // databaseReference.once().then((DataSnapshot snapshot) {
   //   print('Data : ${snapshot.value}');
   // });
-  String result = (await FirebaseDatabase.instance.reference().child("fromPython").once()).value;
-
+  String result = (await FirebaseDatabase.instance.reference().child("fromPython/log/message").once()).value;
+  print(result);
   //remove to and from python descriptors---------------------------------------------------------------------------------uncomment later
   //databaseReference.child('fromPython').remove();
   //databaseReference.child('toPython').remove();
